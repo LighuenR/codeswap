02bd626d475c6ae483e7423607ab22bb57ba83d9
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-12-17 16:40:58 -0300

    Web3 modal button added

--- app.vue
+++ app.vue
@@ -1,30 +1,50 @@
 <script setup>
-const links = [{
-  label: 'Features',
-  to: '#features',
-  exactHash: true
-}, {
-  label: 'Documentation',
-  to: 'https://ui.nuxt.com/pro',
-  target: '_blank'
-}]
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/vue";
+import { mainnet, arbitrum } from "viem/chains";
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "YOUR_PROJECT_ID";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+const links = [
+  {
+    label: "Features",
+    to: "#features",
+    exactHash: true,
+  },
+  {
+    label: "Documentation",
+    to: "https://ui.nuxt.com/pro",
+    target: "_blank",
+  },
+];
 
 useHead({
-  title: 'Nuxt UI Pro - Starter',
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  title: "Nuxt UI Pro - Starter",
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
 useSeoMeta({
-  description: 'Nuxt UI Pro is a collection of premium components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes.'
-})
+  description:
+    "Nuxt UI Pro is a collection of premium components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes.",
+});
 </script>
 
 <template>
@@ -35,8 +55,16 @@ useSeoMeta({
 
     <template #right>
       <UColorModeButton />
+      <w3m-button />
 
-      <UButton to="https://github.com/nuxt-ui-pro/starter" target="_blank" icon="i-simple-icons-github" aria-label="GitHub" color="gray" variant="ghost" />
+      <UButton
+        to="https://github.com/nuxt-ui-pro/starter"
+        target="_blank"
+        icon="i-simple-icons-github"
+        aria-label="GitHub"
+        color="gray"
+        variant="ghost"
+      />
     </template>
   </UHeader>
 
@@ -45,8 +73,6 @@ useSeoMeta({
   </UMain>
 
   <UFooter>
-    <template #left>
-      Copyright © {{ new Date().getFullYear() }}
-    </template>
+    <template #left> Copyright © {{ new Date().getFullYear() }} </template>
   </UFooter>
 </template>
